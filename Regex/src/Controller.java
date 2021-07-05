d647e13c6cee6fcd6aeda19d53a32d4080ff4284
Kolomietsml <kolomietsml17@gmail.com>
2021-07-05 10:13:45 +0300

    add

--- Regex/src/Controller.java
+++ Regex/src/Controller.java
@@ -1,3 +1,64 @@
-public class Controller {
+import java.util.Scanner;
+import java.util.regex.Matcher;
 
+public class Controller {
+    private View view;
+    private Model model;
+    private Scanner scanner;
+
+    public Controller(View view, Model model) {
+        this.view = view;
+        this.model = model;
+        scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        PhoneBook phoneBook = new PhoneBook();
+        String exit = "";
+        while (!exit.equals("e")) {
+            Contact contact = new Contact();
+            addSurname(contact);
+            addNickname(contact);
+            addEmail(contact);
+            view.printMessage(View.EXIT);
+            exit = scanner.next();
+        }
+        view.printMessage(View.ALL_CONTACTS + " " + phoneBook.toString());
+    }
+
+    public void addSurname(Contact contact) {
+        view.printMessage(View.ENTER_SURNAME);
+        String surname = scanner.next();
+        Matcher m = MyPattern.SURNAME_PATTERN.matcher(surname);
+        if (m.matches() == false) {
+            view.printMessage(View.ERROR);
+            addSurname(contact);
+        } else {
+            contact.setSurname(surname);
+        }
+    }
+
+    public void addNickname(Contact contact) {
+        view.printMessage(View.ENTER_NICKNAME);
+        String nickname = scanner.next();
+        Matcher m = MyPattern.NICKNAME_PATTERN.matcher(nickname);
+        if (m.matches() == false){
+            view.printMessage(View.ERROR);
+            addNickname(contact);
+        } else {
+            contact.setNickname(nickname);
+        }
+    }
+
+    public void addEmail(Contact contact) {
+        view.printMessage(View.ENTER_EMAIL);
+        String email = scanner.next();
+        Matcher m = MyPattern.EMAIL_PATTERN.matcher(email);
+        if (m.matches() == false){
+            view.printMessage(View.ERROR);
+            addEmail(contact);
+        } else {
+            contact.setEmail(email);
+        }
+    }
 }
--- Regex/src/View.java
+++ Regex/src/View.java
@@ -1,2 +1,13 @@
 public class View {
+    public static final String ENTER_SURNAME = "Enter surname:";
+    public static final String ENTER_NICKNAME = "Enter nickname:";
+    public static final String ENTER_EMAIL = "Enter email";
+    public static final String ERROR = "Incorrect input enter again";
+    public static final String EXIT = "For exit enter 'e'. if you want to continue click any button.";
+    public static final String ALL_CONTACTS = "Your contacts: ";
+
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+
 }
