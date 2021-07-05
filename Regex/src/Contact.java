c526f111f09732223859a7ede5035dbb410d4220
Kolomietsml <kolomietsml17@gmail.com>
2021-07-05 10:14:46 +0300

    add

---
+++ Regex/src/Contact.java
@@ -0,0 +1,47 @@
+public class Contact {
+    private String surname;
+    private String nickname;
+    private String email;
+
+    public Contact() {
+    }
+
+    public Contact(String surname, String nickname, String email) {
+        this.surname = surname;
+        this.nickname = nickname;
+        this.email = email;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "surname='" + surname + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ Regex/src/MyPattern.java
@@ -0,0 +1,7 @@
+import java.util.regex.Pattern;
+
+public class MyPattern {
+    public static final Pattern SURNAME_PATTERN = Pattern.compile("[A-Z][a-z]{2,}");
+    public static final Pattern NICKNAME_PATTERN = Pattern.compile("[A-Za-z0-9]{2,}");
+    public static final Pattern EMAIL_PATTERN = Pattern.compile("[A-Za-z0-9]{3,30}[@][a-z]{3,10}[.][a-z]{2,4}");
+}
